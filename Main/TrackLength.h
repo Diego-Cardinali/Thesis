bec5bdea46b356b3ea2ef10d5174aa098271ed17
Diego-Cardinali <diego.cardi.98@gmail.com>
2020-09-17 23:00:22 +0200

    New code, cleaned CMake

--- Main/TrackLength.h
+++ Main/TrackLength.h
@@ -8,46 +8,64 @@
 //lower of Threshold. All even positions in Out are segments for which the data is greater
 //or equal to Threshold, if the data starts with a portion  that is lower the first element
 //will be a 0. The return value is the size of Out.
+//The state (high or low) is changed only if a discrepancy is found long at least Discontinuity
 template<typename InIt, typename OutIt, typename T> 
-size_t TrackLength( InIt Begin, InIt End, OutIt Out, T Threshold ) {
+size_t TrackLength( InIt Begin, InIt End, OutIt Out, T Threshold, size_t Discontinuity = 0) {
     size_t CntIns {};
-    size_t n {};
-    if ( Begin != End ) {
-        if ( *Begin < Threshold ) {
+    size_t N {};
+    size_t D {};
+    if (Begin != End) {
+        if (*Begin < Threshold) {
             //If starting data is Low
             *Out++ = {};
             ++CntIns;
         }
         else {
             ++Begin;
-            ++n;
+            ++N;
         }
-        while ( Begin != End ) {
-            if ( CntIns % 2 ) {
+        while (Begin != End) {
+            if (CntIns % 2) {
                 // Low
-                if ( *Begin < Threshold ) {
-                    ++n;
+                if (*Begin < Threshold) {
+                    ++N;
+                    D = 0;
                 }
                 else {
-                    *Out++ = n;
-                    ++CntIns;
-                    n = 1;
+                    if (D < Discontinuity) {
+                        ++D;
+                        ++N;
+                    }
+                    else {
+                        *Out++ = N-D;
+                        ++CntIns;
+                        N = D+1;
+                        D = 0;
+                    }
                 }
             }
             else {
                 // High
-                if ( *Begin >= Threshold ) {
-                    ++n;
+                if (*Begin >= Threshold) {
+                    ++N;
+                    D = 0;
                 }
                 else {
-                    *Out++ = n;
-                    ++CntIns;
-                    n = 1;
+                    if (D < Discontinuity) {
+                        ++D;
+                        ++N;
+                    }
+                    else {
+                        *Out++ = N-D;
+                        ++CntIns;
+                        N = D+1;
+                        D = 0;
+                    }
                 }
             }
             ++Begin;
         }
-        *Out++ = n;
+        *Out++ = N;
     }
     return CntIns;
 }
